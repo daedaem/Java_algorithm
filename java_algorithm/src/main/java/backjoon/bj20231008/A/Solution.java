6a543ea70c018906ff68b776736d8af8ac922c77
조해성 <tg8685@gmail.com>
2023-10-08 17:52:04 +0900

    Feat: add - mac algorithm project setting

---
+++ java_algorithm/src/main/java/backjoon/bj20231008/A/Solution.java
@@ -0,0 +1,7 @@
+package backjoon.bj20231008.A;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ java_algorithm/src/main/java/backjoon/bj20231008/B/B.java
@@ -0,0 +1,7 @@
+package backjoon.bj20231008.B;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println("B번 문제요");
+    }
+}
